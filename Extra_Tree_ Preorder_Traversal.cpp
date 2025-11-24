73f396101a86f3a56e1a76b9bdf158056c11093c
Tuan Huynh <131468167+huynht67@users.noreply.github.com>
2025-11-24 01:23:43 -0600

    Add files via upload

---
+++ Extra_Tree_ Preorder_Traversal.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void preOrder(Node *root) {
+        if (root == nullptr)
+        return;
+    
+    stack<Node*> s;
+    s.push(root);
+    
+        while (!s.empty()) {
+            Node* current = s.top();
+            s.pop();
+            
+            cout << current->data << " ";
+            
+            
+            if (current->right)
+                s.push(current->right);
+            if (current->left)
+                s.push(current->left);
+        }
+    }
+
+}; //End of Solution
